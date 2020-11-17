f1e4e68cd27e069d67c48845ddc6c72c403a62d3
ando-huang <ahuang09182000@gmail.com>
2020-11-17 13:14:26 -0500

    temp sol

---
+++ easy/Reverse.py
@@ -0,0 +1,14 @@
+#this is a solution to reverse on leetcode
+
+int reverse(int x):
+    string = [int(d) for d in str(x)]
+    rev = []
+    if x < 0:
+        rev.append["-"]
+
+    string = string[::-1]
+    for d in string:
+        rev.append(d)
+    
+    
+    return int(str(rev))
