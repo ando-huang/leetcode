d30c1a67d8f19121e486b20a093f3bc2ff5e7caf
ando-huang <ahuang09182000@gmail.com>
2021-01-11 14:29:20 -0800

    sol

---
+++ easy/deletenode.py
@@ -0,0 +1,20 @@
+'''
+    36ms, faster than 83.66% of python sols
+    14.8MB, less than 57.72% of python sols
+'''
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val
+        node.next = node.next.next
+        
