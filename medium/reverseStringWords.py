5f869b0cfd7526a0edae01c009efd5fadd0b5021
ando-huang <ahuang09182000@gmail.com>
2021-02-14 18:36:15 -0500

    solution

---
+++ medium/reverseStringWords.py
@@ -0,0 +1,10 @@
+'''
+    32ms, faster than 74.16% of python sols
+    14.3Mb, less than 88.69% of python sols
+'''
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        l = l.split()
+        l.reverse()
+        return " ".join(l)
